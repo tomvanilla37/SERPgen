cc2e7d04abfa6e0a244fc943977ad408197cef2c
Alireza Habibi <habibi.alireza2010@gmail.com>
2022-05-30 14:28:34 +0200

    prepare to code and UI

--- app.py
+++ app.py
@@ -1,48 +1,17 @@
 import streamlit as st
 
 '''
-# TaxiFareModel front
+# SERPgen front
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
+def key_to_text(key_list):
+    '''
+    a funtion to get the list of keys and return a text
+    input:
+        key_list: list of str
+    output
+        text_out: str
+    '''
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
-url = 'https://taxifare.lewagon.ai/predict'
-
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+    text='dummy'
+    return text
