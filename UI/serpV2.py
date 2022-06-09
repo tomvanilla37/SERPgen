cf496d9d1bc6d13bd270e76f6db7db23057b183e
Alireza Habibi <87390313+AlirezaHabibi2010@users.noreply.github.com>
2022-06-09 09:20:57 +0200

    Update serpV2.py

--- UI/serpV2.py
+++ UI/serpV2.py
@@ -9,59 +9,78 @@ from serp_backend import key_to_text, gen_SERP_single, gen_SERP_mass
 from gstyle_output import gen_SERP_preview
 #import serp_intro
 
-
-
-
 st.set_page_config(page_title="SERPgen - Just Snip It!")
+#st.image(image='./UI/cut_logo.png', use_column_width = True)
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.write("")
+with col2:
+    st.write("")
+with col3:
+    st.image(image='./UI/cut_logo.png', use_column_width = True)
 
-company_name = st.text_input('My Company is called:', key='com_nam')
-purpose_type = st.radio('Purpose', ['selling goods', 'promoting services'])
-input_mod = st.radio('Single query or mass query via csv-file?', ['Single', 'CSV-Upload'])
+nam, claim = st.columns(2)
+with nam:
+    company_name = st.text_input('My company is called:', key='com_nam')
+with claim:
+    company_claim = st.text_input('My company\'s claim is:', help= 'short description/slogan or claim of your company', key='com_cla')
+purp, mod = st.columns(2)
+with purp:
+    purpose_type = st.radio('Purpose', ['selling goods', 'promoting services'])
+with mod:
+    input_mod = st.radio('Single query or mass query via Excel-file?', ['Single', 'Excel-Upload'])
 if input_mod == 'Single':
-    prod, cat, keys = st.columns(3) #columns
+    prod, cat, keys = st.columns(3)
     with prod:
-        product_name = st.text_input('Provide your product name:', key='prod_nam')
+        product_name = st.text_input('Provide your product/service name:', key='prod_nam')
     with cat:
         product_category = st.text_input('Provide your product category:', help = 'What kind of product are you offering?', key='prod_cat')
     with keys:
         product_attributes = st_tags(
-        label='Please specify some attributes:',
+        label= ('Please specify some attributes:'),
         text ='Press enter to add more',
+        #value = ['unique'],
         #maxtags=3,
         key="keys2")
         st.caption('What kind of product are you offering? Let us know some details!')
     if st.button("Submit your choices!"):
         user_input_vals = {"company_name": company_name,
+                "company_claim": company_claim,
                 "purpose_type": purpose_type,
                 "product_name": product_name,
                 "product_category": product_category,
                 "product_attributes": product_attributes
                 }
-        with st.spinner("Creating your customized SERP Result..."):
-            output_single = gen_SERP_single(user_input_vals)
-            st.write(output_single)  #uncomment later
-            ### google like out put +++###
-            with st.expander('Preview your SERP-output in Google:'):
-                components.html(gen_SERP_preview(user_input_vals, output_single))        #gen_SERP_preview outputs html for preview
-                st.balloons()
+        if product_attributes:
+            with st.spinner("Creating your customized SERP Result..."):
+                output_single = gen_SERP_single(user_input_vals)
+                st.markdown(f'> {output_single}')
+                ### google like out put +++###
+                with st.expander('Preview your SERP-output in Google:'):
+                    components.html(gen_SERP_preview(user_input_vals, output_single))
+                    st.balloons()
+        else: st.write('Please provide some attributes of your product!')
 else:
-    user_csv= st.file_uploader("Please provide your CSV!", type=["csv"])
-    if user_csv:
-        input_df = pd.read_csv(user_csv)
+    user_file= st.file_uploader("Please provide your Excel-file!", type=["xls", "xlsx", "xlsm", "xlsb", "odf", "ods", "odt", "csv"])
+    if user_file:
+        input_df = pd.read_csv(user_file)
         with st.expander('Preview of your file:'):
             st.dataframe(input_df)
-    if st.button('SERP my csv.file!') and user_csv:
+    if st.button('SERP my file!') and user_file:
         if company_name != "":
             user_input_vals = {"company_name": company_name,
-                    "purpose_type": purpose_type,
-                    "input_df": input_df
-                    }
-            with st.spinner("Creating your customized SERPed CSV-file..."):
+                               "company_claim": company_claim,
+                                "purpose_type": purpose_type,
+                                "input_df": input_df
+                                }
+            with st.spinner("Creating your customized SERPed Excel-file..."):
                 output_df = gen_SERP_mass(user_input_vals)
                 output_csv = output_df.to_csv()
                 with st.expander('Preview of your SERPed-file:'):
                     st.dataframe(output_df)
-                st.download_button('Download your "SERPed-CSV"-file', data=output_csv, file_name=f'{company_name}_SERPgen.csv')
+            if output_csv:
+                st.download_button('Download your "SERPed-CSV-file"', data=output_csv, file_name=f'{company_name}_SERPgen.csv')
+
         else: st.write('Please provide your company name!')
 
 #if __name__ == "__main__":
