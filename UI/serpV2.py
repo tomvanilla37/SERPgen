998eaa2eff091f3fe1213e689f164cc7ec075a62
Florian Muellenmeister <80272774+tomvanilla37@users.noreply.github.com>
2022-07-04 14:40:51 +0200

    Update serpV2.py
    added downloadable CSV template with examples
    removed preview of uploaded file (to avoid confusion with preview of output file)
    renamed Excel to CSV

--- UI/serpV2.py
+++ UI/serpV2.py
@@ -6,6 +6,8 @@ import streamlit.components.v1 as components
 from serp_backend import gen_serp_single, gen_serp_mass
 from gstyle_output import gen_serp_preview
 
+SAMPLE_FILE = pd.read_csv("SERPgen_sample.csv").to_csv()
+
 st.set_page_config(page_title="SERPgen - Just Snip It!")
 
 col1, col2, col3 = st.columns(3)
@@ -20,7 +22,7 @@ nam, claim = st.columns(2)
 with nam:
     company_name = st.text_input('Company name:', key='com_nam')
 with claim:
-    company_claim = st.text_input('Company\'s claim:', help= 'short description/slogan or claim of your company', key='com_cla')
+    company_claim = st.text_input('Company claim (optional):', help= 'short description/slogan or claim of your company', key='com_cla')
 purp, mod = st.columns(2)
 with purp:
     purpose_type = st.radio('Purpose', ['selling goods', 'promoting services'])
@@ -47,7 +49,7 @@ if input_mod == 'Single':
                 "product_attributes": product_attributes
                 }
         if company_name and product_name and product_category and product_attributes:
-            with st.spinner("Creating your customized SERP Result..."):
+            with st.spinner("Creating your customized SERP results..."):
                 output_single = gen_serp_single(user_input_vals)
                 st.markdown(f'> {output_single}')
                 ### google like output preview ###
@@ -59,27 +61,28 @@ if input_mod == 'Single':
         else: st.error('Please provide your company/product info!')
 else:
     user_file= st.file_uploader("Please provide your CSV-file!", type=["xls", "xlsx", "xlsm", "xlsb", "odf", "ods", "odt", "csv"])
+    st.download_button('Get CSV template here', data=SAMPLE_FILE, file_name='SERPgen_sample.csv')
     if user_file:
         input_df = pd.read_csv(user_file)
-        if input_df["Attributes"].str.len().max() > 21:
-            st.error('Number of rows/product limited to 20 in free version!')
+        if len(input_df.Attributes) > 10:
+            st.error('Number of rows/product limited to 10 in free version!')
 
         else:
-            with st.expander('Preview of your file:'):
-                st.dataframe(input_df)
-    if st.button('SERP my file!') and user_file:
-        if company_name != "":
-            user_input_vals = {"company_name": company_name,
-                               "company_claim": company_claim,
-                                "purpose_type": purpose_type,
-                                "input_df": input_df
-                                }
-            with st.spinner("Creating your customized SERPed Excel-file..."):
-                output_df = gen_SERP_mass(user_input_vals)
-                output_csv = output_df.to_csv()
-                with st.expander('Preview of your SERPed-file:'):
-                    st.dataframe(output_df)
-            if output_csv:
-                st.download_button('Download your "SERPed-CSV-file"', data=output_csv, file_name=f'{company_name}_SERPgen.csv')
+            #with st.expander('Preview of your file:'):
+            #    st.dataframe(input_df)
+            if st.button('SERP my file!') and user_file:
+                if company_name != "":
+                    user_input_vals = {"company_name": company_name,
+                                    "company_claim": company_claim,
+                                        "purpose_type": purpose_type,
+                                        "input_df": input_df
+                                        }
+                    with st.spinner("Creating your SERPed CSV file..."):
+                        output_df = gen_serp_mass(user_input_vals)
+                        output_csv = output_df.to_csv()
+                        with st.expander('Preview of your SERPed CSV file'):
+                            st.dataframe(output_df)
+                    if output_csv:
+                        st.download_button('Download your "SERPed CSV file"', data=output_csv, file_name=f'{company_name}_SERPgen.csv')
 
-        else: st.write('Please provide your company name!')
+                else: st.write('Please provide your company name!')
